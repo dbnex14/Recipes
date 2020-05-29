bebd52f35e7cc874d96adc93a01eb25e2c4a13d3
dbnex <dbnex14@gmail.com>
2020-05-29 16:12:50 -0700

    Add changes to allow selecting Recipe or Shopping List and change to allow selecting recipe and seeing its detail.  Also add Add functionality to shopping list so we can add new ingredients.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,23 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe(
-      'A test Recipe', 
-      'This is simply a test', 
+    new Recipe('A test Recipe', 'This is simply a test', 
       'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg'
     ),
-    new Recipe(
-      'A test Recipe', 
-      'This is simply a test', 
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg'
+    new Recipe('Recipe 2', 'Recipe 2 test data', 
+      'https://149366112.v2.pressablecdn.com/wp-content/uploads/2020/04/img_4211-2-768x768-1.jpg'
     )
   ];
-
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeWasSelected(recipe: Recipe) {
+    // Note that we could have assigned this recipe to selectedRecipe
+    // inside the template as well where we listen to recipeWasSelected
+    // emitted event by simply doing like:
+    // <app-recipe-list (recipeWasSelected)="selectedRecipe = $event"></app-recipe-list>
+    // but it is generally better to not polute template with logic.
+    this.selectedRecipe = recipe;
+  }
 }
