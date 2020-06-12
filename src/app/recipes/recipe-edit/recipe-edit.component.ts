8cde7c24f14190fd07adca8958831fb1e1b3496a
dbnex <dbnex14@gmail.com>
2020-06-12 08:18:05 -0700

    Add ingredients controls (name, amount) array to our recipe-edit base form we created earlier so that for each selected recipe, if we choose to edit it, we now show also the list of ingredients whowing name, amount and an X button for delete.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,6 +15,11 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private recipeService: RecipeService) { }
+
+  // getter to retrieve controls in form generated using reactie approach
+  get formControls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
 
   ngOnInit(): void {
     this.route.params
@@ -36,17 +41,32 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      // If recipe has ingredients, loop through them and for each
+      // create new FormGroup with name and amount controls and push
+      // them onto the array.
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup ({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
   }
 }
